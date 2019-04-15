ca00feb44061c234f8d21f975b539a22b74b9e64
stonewillian <914130243@qq.com>
2019-04-15 21:08:51 +0800

    ARES hs_strncpy hs_snprintf

--- CodeCheck/container/ARESCheck.py
+++ CodeCheck/container/ARESCheck.py
@@ -1,7 +1,36 @@
 #coding:UTF-8
+import re
 import os
 import xml.etree.ElementTree
 import configparser
+from public import glbfunc
+
+def CheckStrNCpy(code):
+    lines = code.split('\n')
+
+    ptn = re.compile(r'hs_strncpy\s*\(@(.*?),.*?,\s*sizeof\s*\(\s*@(.*?)\)(\s*-*\s*1*)\s*\)\s*;')
+    i = 0
+    for line in lines:
+        i += 1
+        vars = re.findall(ptn, line)
+        if vars:
+            for var in vars:
+                if var[0].strip(' ') != var[1].strip(' ') or var[2].replace(' ', '') != '-1':
+                    print('hs_strncpy', i, var)
+
+def CheckSnPrintf(code):
+    lines = code.split('\n')
+
+    ptn = re.compile(r'hs_snprintf\s*\(@(.*?),\s*sizeof\s*\(\s*@(.*?)\)(\s*-*\s*1*)\s*,.*?\)\s*;')
+    i = 0
+    for line in lines:
+        i += 1
+        vars = re.findall(ptn, line)
+        if vars:
+            for var in vars:
+                # print(path + file, i, var)
+                if var[0].strip(' ') != var[1].strip(' ') or var[2].replace(' ', '') != '':
+                    print('hs_snprintf', i, var)
 
 class AresCheck:
     def __init__(self):
@@ -9,7 +38,15 @@ class AresCheck:
         self.ConfigFile.read(r'config\ARESCheckConfig.ini')
 
     def DealFile(self, file):
-        print(file)
+        Root = xml.etree.ElementTree.parse(file).getroot()
+
+        codeNode = Root.find('code')
+        if codeNode != None:
+            code = codeNode.text
+            if code != None and code != '':
+                code = glbfunc.RemoveNote(code).replace('&#xD;', '').replace('&gt;', '>').replace('&lt;', '<')
+                CheckStrNCpy(code)
+                CheckSnPrintf(code)
 
     def DealPath(self,filePath):
         if filePath == '':
